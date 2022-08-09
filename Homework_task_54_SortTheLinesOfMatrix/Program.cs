ee9add2c2a5de8a492a9668cb5bfa5aa9a601d50
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-08-09 19:55:41 +0300

    Part of Homework Seminar 8

---
+++ Homework_task_54_SortTheLinesOfMatrix/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 54: 
+// Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой с
+// троки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int m = 3;
+int n = 4;
+int[,] array = new int[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i,j] = new Random().Next(10);
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortTheLines(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = j+1; k < n; k++)
+            {
+                if (array[i,k] > array[i,j])
+                {
+                    int tmp = array[i,j];
+                    array[i,j] = array[i,k];
+                    array[i,k] = tmp;
+                }
+                
+            }
+            
+        }
+        
+    }
+}
+
+SortTheLines(array);
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
---
+++ Homework_task_56_FindTheSmallestSumByLines/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 56: 
+// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить 
+// строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int m = 4;
+int n = 4;
+int[,] array = new int[m,n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i,j] = new Random().Next(10);
+        Console.Write($"{array[i,j]} ");    
+    }
+    Console.WriteLine();
+}
+
+void FindSum(int[,] array)
+{
+    int sum = 0;
+    int[] sumLines = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            sum += array[i,j];            
+        }
+        //Console.Write($"{sum} ");
+        sumLines[i] = sum;
+        sum = 0;
+    }
+
+    int indexMinSum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if(sumLines[i]<sumLines[indexMinSum]) indexMinSum = i;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Индекс строки с миниимальной суммой элементов в данном массиве равен {indexMinSum}.");
+}
+
+FindSum(array);
